a7018f71aa6707b7d7e97768875de950e234022c
jordidelafuente <jordi.delafuente@gmail.com>
2019-04-10 20:13:07 +0200

    Fondo Main Menu (bolas cayendo)

---
+++ Assets/Scripts/MainMenu/BurbujaFondo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurbujaFondo : MonoBehaviour
+{
+    float timeCreacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCreacion = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.tag == "BurbujaFondo" && Time.time - timeCreacion > 15)
+        {
+            this.gameObject.SetActive(false);
+            Destroy(this.gameObject);
+        }   
+    }
+}
---
+++ Assets/Scripts/MainMenu/FondoTitulo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FondoTitulo : MonoBehaviour
+{
+    public Transform burbuja;
+    float timeBolaAnterior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - timeBolaAnterior > 1.5)
+        {
+            generarBurbuja();
+            timeBolaAnterior = Time.time;
+        }
+        
+    }
+
+    void generarBurbuja()
+    {
+        int xRandom = (int)Random.Range(0 + Camera.main.pixelWidth / 10, Camera.main.pixelWidth - (Camera.main.pixelWidth / 10));
+        Vector3 posicion = new Vector3(xRandom, Camera.main.pixelHeight - 1, 0);
+        posicion = Camera.main.ScreenToWorldPoint(posicion);
+        posicion.z = 0;
+
+        Transform burbujaNueva = Instantiate(burbuja, posicion, Quaternion.identity);
+        burbujaNueva.tag = "BurbujaFondo";
+
+        float scale = Random.Range(0.3f, 1f);
+        burbujaNueva.transform.localScale = new Vector3(scale, scale, scale);
+        burbujaNueva.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -1, 0);
+    }
+}
