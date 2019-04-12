838a8d117e23501e363aef85451c7a698dfa3c20
jordidelafuente <jordi.delafuente@gmail.com>
2019-04-12 21:07:38 +0200

    Mejora UI (User Interface) con 'Pixel UI' v 1.0

---
+++ Assets/Scripts/MainMenu/BotonPlayAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonPlayAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = transform.localScale +
+            new Vector3(0.005f * (Mathf.Sin(Time.time * 2)), 0.005f * (Mathf.Sin(Time.time * 2)), 0);
+    }
+}
